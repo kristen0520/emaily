8fb7b786f5a35cc30710f84e5b0cf8385c5f8bdd
kristen <kristen@example.com>
2017-12-20 16:19:34 -0500

    added google authentication login

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,22 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys')
+
+//trying to add promise
+ mongoose.Promise = global.Promise;
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user)
+  })
+})
 
 passport.use(
   new GoogleStrategy(
@@ -10,8 +26,43 @@ passport.use(
     callbackURL: "/auth/google/callback"
   },
 (accessToken, refreshToken, profile, done) => {
-  console.log('access token', accessToken);
-  console.log('refresh token', refreshToken);
-  console.log('profile:', profile);
+  User.findOne({ googleId: profile.id })
+  .then( (existingUser) => {
+    if(existingUser){
+      //we already have a record witht he given profileid
+      done(null, existingUser)
+    } else {
+      //we dont have record witht his id ake new
+      new User({ googleId: profile.id }).save()
+      .then(user => done(null, user))
+    }
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /*  .then((existingUser) => {
+    if (existingUser) {
+      //we laready have recorde with profileid
+      done(null, existingUser);
+    }
+  else {
+      //we dont have this id on record make new user
+      User.findOne({ googleId: profile.id }).save()
+      .then(user => done(null, user));
+    }
+  })
+  ...this is where its messing up... */
+
 })
 );
