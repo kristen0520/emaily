93a7cb4003fb60931dc3392bf93f73681049b4b7
kristen <kristen@example.com>
2018-01-26 10:20:49 -0500

    auth header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component {
+  renderContent() {
+
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+      default:
+        return <li><a>Logout</a></li>;
+    }
+  }
+
+
   render() {
     return (
       <div>
-        Header
+        <nav
+          className={[
+            "navbar",
+            "navbar-expand-lg",
+            "navbar-dark",
+            "bg-primary"
+          ].join(" ")}
+        >
+          <a className="navbar-brand" href="#">
+            Emaily
+          </a>
+          <ul className="navbar-nav">{this.renderContent()}</ul>
+        </nav>
       </div>
-    )
+    );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(null, mapStateToProps)(Header);
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import {createStore, applyMiddleware } from 'redux'
+import reduxThunk from 'redux-thunk';
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './components/App';
 import reducers from './reducers'
-const store = createStore(reducers, {}, applyMiddleware() );
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk) );
 
 ReactDOM.render(
   <Provider store={store}><App /></Provider>,
