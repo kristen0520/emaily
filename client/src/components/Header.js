3e06fcc22430658f4cc5b35974da29074f53ef94
kristen <kristen@example.com>
2018-01-28 20:00:12 -0500

    added functionality to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,17 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom"
 
 class Header extends Component {
   renderContent() {
-
-    switch (this.props.auth) {
-      case null:
-        return;
-      case false:
-        return
-          <li>
-            <a href="/auth/google">Login With Google</a>
-          </li>
-      default:
-        return <li><a>Logout</a></li>;
-    }
+       switch (this.props.auth) {
+         case null:
+          return;
+         case false:
+          return <a className="nav-link" href="/auth/google">Login With Google</a>
+         default:
+          return <a className="nav-link" href="/api/logout">Logout</a>
+       }
   }
 
 
@@ -29,9 +26,11 @@ class Header extends Component {
             "bg-primary"
           ].join(" ")}
         >
-          <a className="navbar-brand" href="#">
+          <Link
+          to={this.props.auth ? '/surveys' : '/'} 
+            className="navbar-brand">
             Emaily
-          </a>
+          </Link>
           <ul className="navbar-nav">{this.renderContent()}</ul>
         </nav>
       </div>
@@ -43,4 +42,4 @@ function mapStateToProps({ auth }) {
   return { auth };
 }
 
-export default connect(null, mapStateToProps)(Header);
+export default connect(mapStateToProps)(Header);
